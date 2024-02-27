3f204f4e0d73e40ed7beb17a04f311c3186c63ec
semi <semi@OL007385>
2024-02-27 17:45:41 +0200

    Add project files.

---
+++ staffRegister/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staffRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int n = 3;
+
+            for (int i = 0; i < n; i++)
+
+            { 
+                // using the classes
+            employee cStaff = new employee();
+            Console.Write("Enter first name :");
+            cStaff.firstName = Console.ReadLine();
+
+            Console.Write("Enter Last name :");
+            cStaff.lastName = Console.ReadLine();
+
+            salary cSalary = new salary();
+            Console.Write("Enter salary ");
+            cSalary.amount = Console.Read();
+
+                
+             Console.WriteLine("{0}, {1}, {2}", cStaff.firstName, cStaff.lastName, cSalary.amount);
+
+       
+
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+        // Creating classes employee with two attributes: first name and last name
+            class employee
+        {
+            public string firstName { get; set;}
+            public string lastName { get; set;}
+           
+        }
+    // Creating class salary with one attribute amount
+    class salary
+        {
+        public int amount { get; set; }
+        }
+
+
+
+
+
+}
